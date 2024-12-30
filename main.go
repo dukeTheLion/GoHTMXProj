8593ebfe535d21172e175f1eb7d19c0291656e70
Savio G. Mendonça (DUKE) <savio_gm@icloud.com>
2024-12-30 18:09:28 -0300

    1

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
 
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmplt := template.Must(template.ParseFiles("index.html"))
+
+		films := map[string][]Film{
+			"Films": {
+				{Title: "A", Director: "I"},
+				{Title: "B", Director: "J"},
+				{Title: "C", Director: "K"},
+			},
+		}
+
+		tmplt.Execute(w, films)
+	}
+
+	http.HandleFunc("/", h1)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
