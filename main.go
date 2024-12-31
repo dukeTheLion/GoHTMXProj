cce6d874a987d1ba2c40137d6c26d45ebfb40d8b
Savio G. Mendonça (DUKE) <savio_gm@icloud.com>
2024-12-31 08:40:09 -0300

    4

--- main.go
+++ main.go
@@ -1,42 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 )
 
-type Film struct {
-	Title    string
-	Director string
+type Pet struct {
+	Value string
+	Name  string
 }
 
 func main() {
 
-	films := map[string][]Film{
-		"Films": {
-			{Title: "A", Director: "I"},
-			{Title: "B", Director: "J"},
-			{Title: "C", Director: "K"},
+	pets := map[string][]Pet{
+		"Pets": {
+			{Value: string(byte(1)), Name: "I"},
+			{Value: string(byte(2)), Name: "J"},
+			{Value: string(byte(3)), Name: "K"},
 		},
 	}
 
 	h1 := func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.Execute(w, films)
+		tmpl := template.Must(template.ParseGlob("templates/*.html"))
+		tmpl.ExecuteTemplate(w, "index.html", pets)
 	}
 
 	h2 := func(w http.ResponseWriter, r *http.Request) {
-		title := r.PostFormValue("title")
-		director := r.PostFormValue("director")
+		slt := r.PostFormValue("pets")
+		frm := r.PostFormValue("frm")
 
-		films["Films"] = append(films["Films"], Film{Title: title, Director: director})
+		fmt.Println([]byte(slt))
+		fmt.Println([]byte(frm))
 
-		tmpl := template.Must(template.ParseFiles("index.html"))
-		tmpl.ExecuteTemplate(w, "film-list-elements", films)
+		h1(w, r)
 	}
 
 	http.HandleFunc("/", h1)
-	http.HandleFunc("/add-film/", h2)
+	http.HandleFunc("/test/", h2)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
